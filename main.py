712b88f1e8a6b577beb7ad152759dc1ea20e59a4
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2020-09-11 09:20:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-#start all files with import machine
+#start all files with import machine this is where you will import your libraries
 import machine
-
+import time
 
 #create a variable and give it a pin out
+led = machine.Pin(23, machine.Pin.OUT)
 
+#yay you created your first variable in python!
+
+#now we are going to turn the pin on wait 5 seconds then turn it off Put in a while true loop
+while True:
+      led.on()
+      time.sleep(5)
+      led.off
