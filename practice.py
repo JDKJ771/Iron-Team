d658e6d6949e5305e7d8fedbaa46b56772456fdb
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2020-09-13 15:14:05 -0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+#Nellie
 import machine
 
 
@@ -19,3 +20,30 @@ else:
  Leaf.on()
  if red()==1:
      Blue.off()
+
+   
+   #Jed
+   
+   import machine
+
+blue = machine.Pin(23, machine.Pin.OUT)
+green = machine.Pin(21, machine.Pin.OUT)
+redbutton = machine.Pin(22, machine.Pin.IN)
+bluebutton = machine.Pin(19, machine.Pin.IN)
+
+redbutton.value()
+bluebutton.value()
+
+
+while True:
+  if redbutton() == 1:
+    blue.off()
+    
+  else:
+    blue.on()
+    
+  if bluebutton() == 1:
+    green.off()
+    
+  else:
+    green.on()
